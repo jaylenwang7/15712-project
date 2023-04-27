c66660694e057142e6f801fcd21e2499ac469332
jaylenwang7 <jaylencwang@gmail.com>
2023-04-27 13:18:15 -0400

    fix makefile

--- ebpf_ping/test_ping.py
+++ ebpf_ping/test_ping.py
@@ -1,13 +1,15 @@
 import subprocess
 
 def run_cmd(cmd):
-    return subprocess.run(cmd, shell=True, capture_output=True).stdout.decode()
+    return subprocess.run(cmd, shell=True).stdout.decode("utf-8")
 
 def setup_ping():
     # get the network interface name
     device = run_cmd("ip route | grep 10.10.1.1 | awk '{print $3}'").strip()
     run_cmd(f"export DEVICE={device}")
-    run_cmd("make bpf.o && make qdisc && make run")
+    run_cmd("make bpf.o DEVICE=$DEVICE")
+    run_cmd("make qdisc DEVICE=$DEVICE")
+    run_cmd("make run DEVICE=$DEVICE")
 
 def main():
     setup_ping()
