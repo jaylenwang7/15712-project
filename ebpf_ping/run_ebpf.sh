64f3051658ea54d47b89838f0c99d7ae1e1c2e9c
jaylenwang7 <jaylencwang@gmail.com>
2023-04-30 16:37:40 -0400

    fix multiline ip

--- ebpf_ping/run_ebpf.sh
+++ ebpf_ping/run_ebpf.sh
@@ -6,12 +6,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# set the DEVICE variable to the name of your network interface
-DEVICE=$(ip route | grep ${1} | awk '{print $3}')
-echo "DEVICE: $DEVICE"
-
-# set environment variable of the device name
-export DEVICE=$DEVICE
+# set the DEVICE variable to the name of your network interface - take first line of grep output
+LINES=$(ip route | grep ${1})
+# discard lines with "default" in them
+LINES=$(echo "$LINES" | grep -v "default")
+# take first line of output
+DEVICE=$(echo "$LINES" | head -n 1 | awk '{print $3}')
+echo $DEVICE
 
 # make scripts
 make bpf.o DEVICE=$DEVICE
